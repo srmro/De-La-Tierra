d35398deb90f45628bff503a22c4a90ecdc86242
smro <833stephanie@gmail.com>
2025-03-18 12:43:35 -0600

    basic logic for log in page

--- frontend/pages/LogIn.jsx
+++ frontend/pages/LogIn.jsx
@@ -3,21 +3,28 @@ import { useState } from 'react'
 import Navbar from '../components/Navbar'
 import logo from '../pictures/logo.png'
 
-
+//will add a link on signup to take to signup page
 const LogIn = () => {
     return (
         <div>
             <Navbar />
-            <div className='loginContainer'>
+        
                 <div className='topContainer'>
                     <img src={logo} alt='The webiste logo that displays a plant' className='logo' />
                     <h2>Welcome Back!</h2>
-                    <h4>Don't have an account?</h4>
+                    <h4>Don't have an account? Sign up!</h4>
                 </div>
+                <div className='loginContainer'>
                 <form>
-                <label>Enter email:</label> <br/>
-                <label>Enter password:</label> <br/>
-                <button type='submit'>Log In</button>
+                    <label>Enter email:</label>
+                    <input type='email'
+                    placeholder='example@mail.com' />
+                    <br />
+                    <label>Enter password:</label>
+                    <input type='password'
+                    placeholder='Enter Password' />
+                    <br />
+                    <button type='submit'>Log In</button>
 
                 </form>
             </div>
