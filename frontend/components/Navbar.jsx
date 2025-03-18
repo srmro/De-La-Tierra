334c4446eac1521b6e8b6b36d8605cda4a54c224
smro <833stephanie@gmail.com>
2025-03-17 21:22:53 -0600

    temp layout for the navbar

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -1,21 +1,38 @@
 import React from 'react'
 import logo from "../pictures/logo.png"
+import { Link } from 'react-router-dom'
+import NavBar from '../styles/navbar.module.css'
+
+
+
 
 const Navbar = () => {
   return (
-    <nav className='nav-bar'>
-<div className='nav-image'>
-<img src={logo} alt='website logo' />
-</div>
+    <nav className={NavBar.navbar}>
+      <div className={NavBar.navImage}>
+        <img src={logo} alt='website logo' className={NavBar.logo} />
+      </div>
 
-<div className='navbar-center'>
+      <div className={NavBar.navbarCenter}>
+        <ul className={NavBar.navLinks}>
+          <li><a>Home</a></li>
+          <li><a>About</a></li>
+          <li><a>Newsfeed</a></li>
+          <li><a>Starting on a Garden</a></li>
+        </ul>
+      </div>
 
-</div>
+      <div className={NavBar.navbarRight}>
+        <ul className={NavBar.navLinks}>
+          <li><a>Sign up</a></li>
+          <li><a>Log In</a></li>
+        </ul>
+      </div>
 
 
 
-    </nav>
+    </nav >
   )
-}
+};
 
 export default Navbar
--- frontend/pages/Welcome.jsx
+++ frontend/pages/Welcome.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import plantHand from "../pictures/plant-hand.avif"
+import Navbar from '../components/Navbar'
+import welcome from "../styles/welcome.module.css"
 
 const Welcome = () => {
   return (
+    <div>
+      <head>
+    <title>Welcome! 🌱</title>
+      </head>
+<div>
+<Navbar/>
+</div>
+
     <div className='main-header' >
 <h3>Welcome to a site that unites all type of <br/><em>plant hobbyist!</em></h3>
-<h1>De La Tierra</h1>
-<img src={plantHand} alt='Hand line art holding a plant.' />
+<h1 className={welcome}>De La Tierra</h1>
+<img src={plantHand} alt='Hand line art holding a plant.' className={welcome} />
     </div>
+
+
+</div>
   )
 }
 
